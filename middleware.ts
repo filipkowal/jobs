ae11358007430adfcbe70846d30dc7be50adacbc
Filip Kowal <filip.kowal@protonmail.com>
2023-06-19 17:19:18 +0200

    Create i18n starter, add translations

---
+++ middleware.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import { i18n } from "./i18n-config";
+
+import { match as matchLocale } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+function getLocale(request: NextRequest): string | undefined {
+  const negotiatorHeaders: Record<string, string> = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+  let expectedLanguages = new Negotiator({
+    headers: negotiatorHeaders,
+  }).languages();
+  // @ts-ignore locales are readonly
+  const availableLocales: string[] = i18n.locales;
+  return matchLocale(expectedLanguages, availableLocales, i18n.defaultLocale);
+}
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname.includes("/images") || pathname.includes("/fonts")) return;
+
+  const pathnameIsMissingLocale = i18n.locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathname}`, request.url)
+    );
+  }
+}
+
+export const config = {
+  // Matcher ignoring `/_next/` and `/api/`
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
