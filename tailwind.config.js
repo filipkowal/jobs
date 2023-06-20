a91331222c35eff963726cb9da788b9e5b6058be
Filip Kowal <filip.kowal@protonmail.com>
2023-06-20 18:43:25 +0200

    Add tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
+const customBoard = require("./customBoard.json");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: customBoard.colors,
+      screens: {
+        "2xl": "1536px",
+        "3xl": "1920px",
       },
     },
+    fontFamily: {
+      sans: ["var(--font-inter)", "sans-serif"],
+      title: ["var(--font-stolzl)", "sans-serif"],
+      serif: ["var(--font-merriweather)", "serif"],
+      logo: ["var(--font-loew)", "sans-serif"],
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar-hide"),
+    require("tailwindcss-animate"),
+  ],
+};
