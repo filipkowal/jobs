c742e9d549391d732941f392881638d9685b2567
Filip Kowal <filip.kowal@protonmail.com>
2023-06-21 14:37:15 +0200

    Remove src from tawilwindcontent path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const customBoard = require("./customBoard.json");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
