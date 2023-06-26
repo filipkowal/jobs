65b4ac1b78028e152188348d64054b68d816f9d5
Filip Kowal <filip.kowal@protonmail.com>
2023-06-26 19:35:03 +0200

    Display 20 jobs on page

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -2,7 +2,7 @@ export const SERVER_URL = "https://merge.digitalent.cloud/api/vertical";
 export const MOCK_SERVER_URL =
   "https://virtserver.swaggerhub.com/filipkowal/jobs/1.0";
 
-export const JOBS_LIMIT = 25;
+export const JOBS_LIMIT = 20;
 export const JOBS_REVALIDATE_TIME = 60 * 60; // 1 hour
 
 export const FILTER_NAMES = [
