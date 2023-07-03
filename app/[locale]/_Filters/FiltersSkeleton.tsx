02fa2bab5beb197a3a9ad92a1a31815f37d2696d
Filip Kowal <filip.kowal@protonmail.com>
2023-07-03 19:48:38 +0200

    Add filters icon skeleton

--- app/[locale]/_Filters/FiltersSkeleton.tsx
+++ app/[locale]/_Filters/FiltersSkeleton.tsx
@@ -2,6 +2,7 @@ export default function FiltersSkeleton() {
   return (
     <div className="lg:w-10/12 w-full max-w-[70rem] justify-left h-[44px]">
       <div className="hidden lg:flex flex-row mb-2 gap-2 flex-wrap relative animate-pulse blur-sm">
+        <span className=" py-1 px-3 text-digitalent-blue">H</span>
         <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
           Berufsfelder
         </span>
