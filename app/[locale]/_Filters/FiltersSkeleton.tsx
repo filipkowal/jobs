7b481add55d53cceed04b4d706a0d62a7c4a8688
Filip Kowal <filip.kowal@protonmail.com>
2023-07-03 19:18:08 +0200

    Make FiltersSkeleton similar to the real one

--- app/[locale]/_Filters/FiltersSkeleton.tsx
+++ app/[locale]/_Filters/FiltersSkeleton.tsx
@@ -1,16 +1,31 @@
 export default function FiltersSkeleton() {
   return (
-    <div className="lg:w-10/12 w-full max-w-[70rem] justify-left">
-      <div className="hidden lg:flex flex-row mb-2 gap-2 flex-wrap relative">
-        <span className="bg-digitalent-gray-light animate-pulse h-10 mr-2 w-8" />
-        <span className="bg-digitalent-gray-light animate-pulse h-10 mr-2 w-32" />
-        <span className="bg-digitalent-gray-light animate-pulse h-10 mr-2 w-32" />
-        <span className="bg-digitalent-gray-light animate-pulse h-10 mr-2 w-32" />
-        <span className="bg-digitalent-gray-light animate-pulse h-10 mr-2 w-32" />
-        <span className="bg-digitalent-gray-light animate-pulse h-10 mr-2 w-32" />
+    <div className="lg:w-10/12 w-full max-w-[70rem] justify-left h-[44px]">
+      <div className="hidden lg:flex flex-row mb-2 gap-2 flex-wrap relative animate-pulse blur-sm">
+        <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
+          Berufsfelder
+        </span>
+        <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
+          Technologien
+        </span>
+        <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
+          Branchen
+        </span>
+        <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
+          Firmengrösse
+        </span>
+        <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
+          Arbeitslevel
+        </span>
+        <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
+          Arbeitspensum
+        </span>
+        <span className="border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue">
+          Mehr...
+        </span>
       </div>
 
-      <span className="block lg:hidden bg-digitalent-gray-light animate-pulse h-10 w-28 mb-2 ml-1" />
+      <span className="block lg:hidden border-digitalent-blue border-2 py-1 px-3 text-digitalent-blue ml-1" />
     </div>
   );
 }
