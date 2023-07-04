fa71be264c9dea64e9bffc1c4f7c64fea4718fb0
Filip Kowal <filip.kowal@protonmail.com>
2023-07-04 14:22:05 +0200

    minimumCachTTl of one day

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    minimumCacheTTL: 60 * 60 * 24,
+    minimumCacheTTL: 86400, // 1 day
     remotePatterns: [
       {
         protocol: "https",
