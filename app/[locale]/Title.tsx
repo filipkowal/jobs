151a5da809bd0718f7d1f3397189288ec1819e48
Filip Kowal <filip.kowal@protonmail.com>
2023-07-04 14:55:40 +0200

    Smaller Title fonts on mobile

--- app/[locale]/Title.tsx
+++ app/[locale]/Title.tsx
@@ -6,14 +6,14 @@ export default async function Title({ locale }: { locale: Locale }) {
   const { title: dict } = await getDictionary(locale);
 
   return (
-    <div className="md:my-40 mb-14 mt-28 text-center">
+    <div className="my-14 mx-3 sm:mt-28 md:my-40 text-center">
       {customBoard.hideTitle ? null : (
-        <h1 className="text-4xl md:text-[3.6rem] font-medium mb-4 font-title leading-snug text-digitalent-blue">
+        <h1 className="text-3xl md:text-[3.6rem] font-medium mb-4 font-title leading-snug text-digitalent-blue">
           {customBoard.title || dict["Finest jobs selection"]}
         </h1>
       )}
       {customBoard.hideSubtitle ? null : (
-        <h2 className="text-xl font-serif font-light text-digitalent-blue">
+        <h2 className="text-lg font-serif font-light text-digitalent-blue">
           {customBoard.subtitle || dict["Interview in 24h"]}
         </h2>
       )}
@@ -23,11 +23,11 @@ export default async function Title({ locale }: { locale: Locale }) {
 
 export function TitleSkeleton() {
   return (
-    <div className="md:my-40 mb-14 mt-28 text-center blur-xl animate-pulse min-h:[123.188px]">
-      <h1 className="text-4xl md:text-[3.6rem] font-medium mb-4 font-title leading-snug text-digitalent-blue">
+    <div className="my-14 mx-3 sm:mt-28 md:my-40 text-center blur-xl animate-pulse min-h:[123.188px]">
+      <h1 className="text-3xl md:text-[3.6rem] font-medium mb-4 font-title leading-snug text-digitalent-blue">
         Handverlesene IT Jobs bei TOP Unternehmen
       </h1>
-      <h2 className="text-xl font-serif font-light text-digitalent-blue">
+      <h2 className="text-lg font-serif font-light text-digitalent-blue">
         Garantiertes Feedback innert 48 Stunden
       </h2>
     </div>
