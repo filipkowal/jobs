f3567b7e98f25e4c2d8039b185ce191655a862d5
Filip Kowal <filip.kowal@protonmail.com>
2023-07-21 12:42:11 +0200

    Prevent wrong locale in getDictionary error

--- utils/server/helpers.tsx
+++ utils/server/helpers.tsx
@@ -1,5 +1,6 @@
 import "server-only";
 import type { Locale } from "../../i18n-config";
+import { i18n } from "../../i18n-config";
 
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
@@ -16,5 +17,6 @@ const customBoard = import("../../customBoard.json").then(
   (module) => module.default
 );
 
-export const getDictionary = async (locale: Locale) => dictionaries[locale]();
+export const getDictionary = async (locale: Locale) =>
+  dictionaries[i18n.locales.includes(locale) ? locale : i18n.defaultLocale]();
 export const getCustomBoard = async () => customBoard;
