d284c8e70b7b620957bb07d2bb45363eb430816c
Filip Kowal <filip.kowal@protonmail.com>
2023-07-21 13:38:31 +0200

    Prevent RangeError of * as expected language

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,13 @@ function getLocale(request: NextRequest): string | undefined {
     ", defaultLocale: ",
     i18n.defaultLocale
   );
-  return matchLocale(expectedLanguages, availableLocales, i18n.defaultLocale);
+  return matchLocale(
+    expectedLanguages?.length === 1 && expectedLanguages[0] === "*" // Negotiator returns ["*"] if no Accept-Language header is present
+      ? ["en"]
+      : expectedLanguages,
+    availableLocales,
+    i18n.defaultLocale
+  );
 }
 
 export function middleware(request: NextRequest) {
