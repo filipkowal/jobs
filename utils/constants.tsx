c6e0ff3a6f8e39f80a4af567a483415698f2866a
Filip Kowal <filip.kowal@protonmail.com>
2023-08-17 11:51:15 +0200

    Change URL of the server to "merge"

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,4 +1,4 @@
-export const SERVER_URL = "https://vertical.digitalent.cloud/api/vertical";
+export const SERVER_URL = "https://merge.digitalent.cloud/api/vertical";
 export const MOCK_SERVER_URL =
   "https://virtserver.swaggerhub.com/filipkowal/jobs/1.0";
 
