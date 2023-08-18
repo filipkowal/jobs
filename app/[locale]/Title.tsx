d79911af6568b5b77de41628205dfec1c68a8ba7
Filip Kowal <filip.kowal@protonmail.com>
2023-08-18 17:48:25 +0200

    Change Title styling

--- app/[locale]/Title.tsx
+++ app/[locale]/Title.tsx
@@ -6,18 +6,18 @@ export default async function Title({ locale }: { locale: Locale }) {
   const { title: dict } = await getDictionary(locale);
 
   return (
-    <div className="sm:mt-28 md:my-40">
+    <div className="sm:mt-28 md:mt-40 md:mb-20">
       {customBoard.hideTitle ? null : (
-        <h1 className="text-3xl md:text-[3.6rem] font-medium mb-4 font-title leading-snug text-digitalent-blue">
+        <h1 className="text-3xl md:text-[3.6rem] font-medium mb-2 font-title leading-snug text-digitalent-blue">
           {dict["title"]}
         </h1>
       )}
       {customBoard.hideSubtitle ? null : (
         <>
-          <h2 className="text-lg font-serif font-light text-digitalent-blue">
+          <h2 className="text-2xl font-title text-digitalent-blue">
             {dict["subtitle.1"]}
           </h2>
-          <h2 className="text-lg font-serif font-light text-white">
+          <h2 className="text-2xl font-title text-white">
             {dict["subtitle.2"]}
           </h2>
         </>
@@ -28,12 +28,15 @@ export default async function Title({ locale }: { locale: Locale }) {
 
 export function TitleSkeleton() {
   return (
-    <div className="sm:mt-28 md:my-40 blur-xl animate-pulse min-h:[123.188px]">
-      <h1 className="text-3xl md:text-[3.6rem] font-medium mb-4 font-title leading-snug text-digitalent-blue">
-        Handverlesene IT Jobs bei TOP Unternehmen
+    <div className="sm:mt-28 md:mt-40 md:mb-20 blur-xl animate-pulse min-h:[123.188px]">
+      <h1 className="text-3xl md:text-[3.6rem] font-medium mb-2 font-title leading-snug text-digitalent-blue">
+        Bereit für eine neue Mission?
       </h1>
-      <h2 className="text-lg font-serif font-light text-digitalent-blue">
-        Garantiertes Feedback innert 48 Stunden
+      <h2 className="text-2xl font-title text-digitalent-blue">
+        Entdecke ausgewählte Karrieremöglichkeiten in Schweizer Top-Unternehmen!
+      </h2>
+      <h2 className="text-2xl font-title text-white">
+        Mit garantiertem Feedback innert 2 Tagen
       </h2>
     </div>
   );
