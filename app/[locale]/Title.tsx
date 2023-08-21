1ca60cdc6b2a6099ecf241dca2e77d70aff5c307
Filip Kowal <filip.kowal@protonmail.com>
2023-08-21 15:05:41 +0200

    Style Title for mobile

--- app/[locale]/Title.tsx
+++ app/[locale]/Title.tsx
@@ -1,43 +1,59 @@
 import { getCustomBoard, getDictionary } from "../../utils/server";
 import { Locale } from "../../i18n-config";
+import { type CustomBoard } from "../../utils";
 
 export default async function Title({ locale }: { locale: Locale }) {
   const customBoard = await getCustomBoard();
   const { title: dict } = await getDictionary(locale);
 
   return (
-    <div className="sm:mt-28 md:mt-40 md:mb-20">
-      {customBoard.hideTitle ? null : (
-        <h1 className="text-3xl md:text-[3.6rem] font-medium mb-2 font-title leading-snug text-digitalent-blue">
-          {dict["title"]}
-        </h1>
-      )}
-      {customBoard.hideSubtitle ? null : (
-        <>
-          <h2 className="text-2xl font-title text-digitalent-blue">
-            {dict["subtitle.1"]}
-          </h2>
-          <h2 className="text-2xl font-title text-white">
-            {dict["subtitle.2"]}
-          </h2>
-        </>
-      )}
-    </div>
+    <TitleLayout
+      title={dict.title}
+      subtitle1={dict["subtitle.1"]}
+      subtitle2={dict["subtitle.2"]}
+      customBoard={customBoard}
+    />
   );
 }
 
 export function TitleSkeleton() {
   return (
-    <div className="sm:mt-28 md:mt-40 md:mb-20 blur-xl animate-pulse min-h:[123.188px]">
-      <h1 className="text-3xl md:text-[3.6rem] font-medium mb-2 font-title leading-snug text-digitalent-blue">
-        Bereit für eine neue Mission?
-      </h1>
-      <h2 className="text-2xl font-title text-digitalent-blue">
-        Entdecke ausgewählte Karrieremöglichkeiten in Schweizer Top-Unternehmen!
-      </h2>
-      <h2 className="text-2xl font-title text-white">
-        Mit garantiertem Feedback innert 2 Tagen
-      </h2>
+    <TitleLayout
+      title="Bereit für eine neue Mission?"
+      subtitle1="Entdecke ausgewählte Karrieremöglichkeiten in Schweizer Top-Unternehmen!"
+      subtitle2="Mit garantiertem Feedback innert 2 Tagen"
+    />
+  );
+}
+
+function TitleLayout({
+  title,
+  subtitle1,
+  subtitle2,
+  customBoard,
+}: {
+  title: string;
+  subtitle1: string;
+  subtitle2: string;
+  customBoard?: CustomBoard;
+}) {
+  return (
+    <div className="mt-16 sm:mt-28 md:mt-40 mb-10 md:mb-20">
+      {customBoard?.hideTitle ? null : (
+        <h1 className="text-3xl md:text-[3.6rem] font-medium mb-2 font-title leading-snug text-digitalent-blue">
+          {title}
+        </h1>
+      )}
+      {customBoard?.hideSubtitle ? null : (
+        <>
+          <h2 className="text-md md:text-2xl font-title text-digitalent-blue">
+            {subtitle1}
+          </h2>
+          <h2 className="text-md md:text-2xl font-title text-white">
+            {subtitle2}
+          </h2>
+        </>
+      )}
     </div>
   );
 }
