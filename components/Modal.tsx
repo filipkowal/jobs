b6e9124f2708b060d96364e2362c366fea7ef719
Filip Kowal <filip.kowal@protonmail.com>
2023-11-03 14:22:26 +0100

    Fix Modal's close button placement

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -53,27 +53,28 @@ export default function Modal({
                 <Dialog.Panel
                   className={`transform overflow-hidden h-full sm:h-initial bg-digitalent-gray-light text-digitalent-blue px-4 py-7 md:py-10 md:px-16 text-left align-middle shadow-xl transition-all w-[60rem] max-w-full`}
                 >
-                  <svg
-                    className="w-6 h-6 absolute right-6 top-6"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                    onClick={closeModal}
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
                   <Dialog.Title
                     as="h3"
-                    className="text-2xl font-title font-medium uppercase pr-10 pb-6"
+                    className="text-2xl font-title font-medium uppercase pb-6 flex justify-between"
                   >
-                    {title}
+                    <span className="pr-10 max-w-[90%]">{title}</span>
+
+                    <svg
+                      className="w-6 h-6 float-right -translate-y-1 cursor-pointer"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                      onClick={closeModal}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
                   </Dialog.Title>
 
-                  <div className="max-h-[76vh] px-1 -mx-1 sm:mx-0 max-w-full overflow-x-hidden overflow-y-auto">
+                  <div className="max-h-[74vh] px-1 -mx-1 sm:mx-0 max-w-full overflow-x-hidden overflow-y-auto">
                     {children}
                   </div>
                   <div className="w-full">{footer}</div>
