a6ce41cf4af36a8f008c24855550c70b2e743a7b
Filip Kowal <filip.kowal@protonmail.com>
2023-11-09 10:40:15 +0100

    Inline address with salary

--- app/[locale]/_JobRow/JobRowHeading.tsx
+++ app/[locale]/_JobRow/JobRowHeading.tsx
@@ -103,9 +103,9 @@ export default async function JobRowHeading({
             </span>
           )}
           {address && (
-            <span className="md:px-4 md:py-2 md:w-28 md:text-right">
+            <span className="md:px-4 md:py-2 md:w-28 md:text-right translate-y-[2px]">
               <Tooltip content={dict["Location"]} ariaLabel="Location">
-                <span className="font-title text-xs md:text-base md:font-medium inline-block w-[100px] whitespace-nowrap text-ellipsis overflow-hidden">
+                <span className="font-title text-xs md:text-base md:font-medium md:leading-none inline-block w-[100px] whitespace-nowrap text-ellipsis overflow-hidden">
                   {`${address?.city || address?.country}`}
                 </span>
               </Tooltip>
