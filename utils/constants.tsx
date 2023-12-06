aba03434bdedd13b5086d410373f0ff4fe64a8ae
Filip Kowal <filip.kowal@protonmail.com>
2023-12-06 19:39:10 +0100

    Customize filters

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,4 +1,5 @@
-export const SERVER_URL = "https://vertical.digitalent.cloud/api/vertical";
+export const SERVER_URL =
+  "https://custom-jb-trial.digitalent.cloud/api/vertical";
 export const MOCK_SERVER_URL =
   "https://virtserver.swaggerhub.com/filipkowal/jobs/1.0";
 
