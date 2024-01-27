f8d4a22212b92c8cb71de4b9d135f8671d018e86
Filip Kowal <filip.kowal@protonmail.com>
2024-01-27 08:56:46 +0100

    Bring Modal foremost

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -26,7 +26,7 @@ export default function Modal({
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" className="relative z-40" onClose={closeModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
