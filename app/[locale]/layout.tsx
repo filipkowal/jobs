9a6a6059c61b8d3871a47f697bfb3633ce96f5d6
Filip Kowal <filip.kowal@protonmail.com>
2024-02-07 15:43:15 +0100

    Remove google analytics script

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -80,14 +80,6 @@ export default async function RootLayout({
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         />
-        <Script id="googleAnalytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
-        </Script>
       </head>
       <body
         style={{
