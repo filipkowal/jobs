825695a47c7faa67b4fec046d8767f08dcf0956c
filipkowal <filip.kowal@protonmail.com>
2024-06-13 21:22:34 +0200

    Move shareJob to its own test file

--- tests/e2e/jobTable/jobTable.spec.ts
+++ tests/e2e/jobTable/jobTable.spec.ts
@@ -15,9 +15,7 @@ test("Job can be opened", async ({ pageWithJob }) => {
   ).toBeVisible();
 });
 
-test("Clicking More Info button has an external link", async ({
-  pageWithJob,
-}) => {
+test("More Info button has an external link", async ({ pageWithJob }) => {
   const { job } = pageWithJob;
   const moreInfoLink = job.getByRole("link", { name: /more info & apply/i });
   await expect(moreInfoLink).toHaveAttribute("href");
@@ -26,50 +24,4 @@ test("Clicking More Info button has an external link", async ({
     "href",
     /.*jobs.digitalent.cloud.*/
   );
-});
-
-test("Clicking Share & Earn button opens a modal", async ({
-  shareModalOpened,
-}) => {
-  const { page } = shareModalOpened;
-  await expect(page.getByText(/share a job/i)).toBeVisible();
-});
-
-test("Share modal button is only enabled if email is valid and terms are accepted", async ({
-  shareModalOpened,
-}) => {
-  const { page } = shareModalOpened;
-  const createLinkButton = page.getByRole("button", { name: /create a link/i });
-  const emailInput = page.getByLabel(/email.*\**/i);
-
-  const termsCheckbox = page.getByRole("checkbox", {
-    name: /i have read and agree to the terms of use\. \*/i,
-  });
-  await termsCheckbox.check();
-
-  // disabled if email is not valid
-  await emailInput.fill("invalidEmail");
-  await expect(createLinkButton).toBeDisabled();
-
-  // enabled if email is valid
-  await emailInput.fill("a@a.de");
-  await expect(createLinkButton).toBeEnabled();
-
-  // disabled if terms are not accepted
-  await termsCheckbox.uncheck();
-  await expect(createLinkButton).toBeDisabled();
-});
-
-test("Creates a share link and copies it", async ({ shareModalWithLink }) => {
-  const { page, shareLinkInput } = shareModalWithLink;
-  await expect(
-    page.getByText(/it's your link\. now you just have to share it\./i)
-  ).toBeVisible();
-  await expect(shareLinkInput).toHaveValue(/http/);
-
-  const copyButton = page.getByRole("button", { name: /copy/i });
-  await copyButton.click();
-
-  const pastedLink = await page.evaluate(() => navigator.clipboard.readText());
-  expect(pastedLink).toContain(await shareLinkInput.inputValue());
 });
---
+++ tests/e2e/jobTable/shareJob.spec.ts
@@ -0,0 +1,48 @@
+import { test } from "./fixtures";
+import { expect } from "@playwright/test";
+
+test("Clicking Share & Earn button opens a modal", async ({
+  shareModalOpened,
+}) => {
+  const { page } = shareModalOpened;
+  await expect(page.getByText(/share a job/i)).toBeVisible();
+});
+
+test("Share modal button is only enabled if email is valid and terms are accepted", async ({
+  shareModalOpened,
+}) => {
+  const { page } = shareModalOpened;
+  const createLinkButton = page.getByRole("button", { name: /create a link/i });
+  const emailInput = page.getByLabel(/email.*\**/i);
+
+  const termsCheckbox = page.getByRole("checkbox", {
+    name: /i have read and agree to the terms of use\. \*/i,
+  });
+  await termsCheckbox.check();
+
+  // disabled if email is not valid
+  await emailInput.fill("invalidEmail");
+  await expect(createLinkButton).toBeDisabled();
+
+  // enabled if email is valid
+  await emailInput.fill("a@a.de");
+  await expect(createLinkButton).toBeEnabled();
+
+  // disabled if terms are not accepted
+  await termsCheckbox.uncheck();
+  await expect(createLinkButton).toBeDisabled();
+});
+
+test("Creates a share link and copies it", async ({ shareModalWithLink }) => {
+  const { page, shareLinkInput } = shareModalWithLink;
+  await expect(
+    page.getByText(/it's your link\. now you just have to share it\./i)
+  ).toBeVisible();
+  await expect(shareLinkInput).toHaveValue(/http/);
+
+  const copyButton = page.getByRole("button", { name: /copy/i });
+  await copyButton.click();
+
+  const pastedLink = await page.evaluate(() => navigator.clipboard.readText());
+  expect(pastedLink).toContain(await shareLinkInput.inputValue());
+});
