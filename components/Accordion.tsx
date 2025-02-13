a2a7ac3afa066aa3fabe31997b5f54792dae0b34
filipkowal <filip.kowal@protonmail.com>
2025-02-13 10:14:31 +0100

    Fix headlessui errors

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { Disclosure } from "@headlessui/react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 import { ReactNode } from "react";
 
@@ -17,11 +21,11 @@ export default function Accordion({
 }) {
   return (
     <div className="w-full py-2">
-      <Disclosure defaultOpen={isOpen}>
+      <Disclosure as="div" defaultOpen={isOpen}>
         {({ open }) => (
           <>
             <div className="flex">
-              <Disclosure.Button className="flex w-full justify-left gap-2 py-2 text-left font-medium -mx-1">
+              <DisclosureButton className="flex w-full justify-left gap-2 py-2 text-left font-medium -mx-1">
                 <ChevronUpIcon
                   className={`${!open ? "rotate-180 transform" : ""} h-7 w-7`}
                 />
@@ -31,11 +35,11 @@ export default function Accordion({
                     {labelTag}
                   </span>
                 )}
-              </Disclosure.Button>
+              </DisclosureButton>
             </div>
-            <Disclosure.Panel className="pl-1 pt-4 pb-2 text-digitalent-blue w-full">
+            <DisclosurePanel className="pl-1 pt-4 pb-2 text-digitalent-blue w-full">
               {children}
-            </Disclosure.Panel>
+            </DisclosurePanel>
           </>
         )}
       </Disclosure>
