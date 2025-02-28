e6b9cd153f35b4636738a106078f7c656229c895
filipkowal <filip.kowal@protonmail.com>
2025-02-28 14:47:25 +0100

    Remove redundant fetchers

--- utils/fetchers.ts
+++ utils/fetchers.ts
@@ -1,4 +1,4 @@
-import type { Endpoint, Jobs, JobsQuery } from "./types";
+import type { JobsQuery } from "./types";
 import { type Locale } from "@/i18n-config";
 import { buildQueryString, createUrl } from "./helpers";
 
@@ -18,7 +18,7 @@ export async function postData({
     locale,
   });
 
-  const rawResponse = await fetch(url, {
+  const response = await fetch(url, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -28,74 +28,12 @@ export async function postData({
     body: JSON.stringify(data),
   });
 
-  if (!rawResponse.ok) {
-    throw new Error(rawResponse.statusText);
+  if (!response.ok) {
+    throw new Error(response.statusText);
   }
 
-  const content = await rawResponse.json();
+  const content = await response.json();
   return content;
-}
-
-async function getData({
-  endpoint,
-  locale,
-  param,
-  searchParams,
-  init = {},
-  boardId,
-}: {
-  endpoint: Endpoint;
-  locale: Locale;
-  param?: string;
-  searchParams?: Record<string, any>;
-  init?: RequestInit;
-  boardId?: string;
-}) {
-  try {
-    const url = createUrl({
-      endpoint,
-      locale,
-      param,
-      searchParams,
-    });
-
-    const res = await fetch(url, {
-      ...init,
-      headers: { ...init.headers, "Board-Id": boardId || "" },
-    });
-
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status} in ${url}`);
-    }
-
-    return res.json();
-  } catch (e: any) {
-    throw Error("Failed fetching " + endpoint + ": " + e.message);
-  }
-}
-
-export async function getJobs({
-  locale,
-  searchParams,
-  init,
-  boardId,
-}: {
-  locale: Locale;
-  searchParams?: JobsQuery;
-  init?: RequestInit;
-  boardId?: string;
-}): Promise<Jobs> {
-  try {
-    return await getData({
-      endpoint: "jobs",
-      locale,
-      searchParams,
-      init,
-      boardId,
-    });
-  } catch (error) {
-    throw error;
-  }
 }
 
 export async function getFilteredJobs({
