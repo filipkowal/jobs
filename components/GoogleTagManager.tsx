ae1c561881de917f1fe5817dd9c48bd40b12c1d3
filipkowal <filip.kowal@protonmail.com>
2025-10-22 01:15:28 +0200

    Add GTM, Remove wrong CSP

--- components/GoogleTagManager.tsx
+++ components/GoogleTagManager.tsx
@@ -6,13 +6,6 @@ import { useCookieConsent } from "@/contexts/CookieConsentContext";
 export default function GoogleTagManager() {
   const { hasConsent } = useCookieConsent();
 
-  // SECURITY ALERT: GTM container GTM-PZR49N2Q appears to be suspicious
-  // Temporarily disabled to prevent potential malicious injection
-  // TODO: Replace with legitimate GTM container ID
-  return null;
-
-  // Original code commented out for security:
-  /*
   if (!hasConsent) return null;
 
   return (
@@ -22,13 +15,14 @@ export default function GoogleTagManager() {
       src={`https://www.googletagmanager.com/gtag/js?id=GTM-PZR49N2Q`}
       onLoad={() => {
         window.dataLayer = window.dataLayer || [];
-        window.gtag = function(){window.dataLayer.push(arguments);}
-        window.gtag('js', new Date());
-        window.gtag('config', 'GTM-PZR49N2Q', {
+        window.gtag = function () {
+          window.dataLayer.push(arguments);
+        };
+        window.gtag("js", new Date());
+        window.gtag("config", "GTM-PZR49N2Q", {
           page_path: window.location.pathname,
         });
       }}
     />
   );
-  */
 }
