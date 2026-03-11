d3fcb44138b0f753669f48352fc48b00b8c077ef
filipkowal <filip.kowal@proton.me>
2026-03-11 23:54:15 +0100

    Upgrade other packages major versions

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default [
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+];
